139435d9aa15aa7e56123590ed0a5fb553eaa1a8
RNV Sundeep <routulasundeep@msitprogram.net>
2020-04-20 12:26:50 +0530

    route changes

--- Project1/project1/application.py
+++ Project1/project1/application.py
@@ -1,10 +1,10 @@
 import os
 import datetime
 from dotenv import load_dotenv
-from models import Users
+from models import *
 
 from flask import Flask, session, render_template, request
-from flask_session import Session
+# from flask_session import Session
 from sqlalchemy import *
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -19,7 +19,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-Session(app)
+app.secret_key="SECRET_KEY"
+
+# Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
@@ -33,48 +35,47 @@ def main():
 
 @app.route("/register", methods=["GET","POST"])
 def register():
-    print("starting", session)
-    if 'username' in session:
-        username = session["username"]
-        return (f"<h1>Logged in as {username}</h1>" + "<b><a href = '/dropsession'>click here to log out</a></b>")
-
     if request.method == "GET":
         alert = False
-        return render_template("login.html", alert=alert)
+        return render_template("registration.html", alert=alert)
     
     elif request.method == "POST":
-        if (request.form["button"] == "register"):
-            return render_template("registration.html")
-        
-        elif (request.form["button"] == "submit"):
-            name = request.form.get("name")
-            username = request.form.get("username")
-            password = request.form.get("password")
-            birthday = request.form.get("birthday")
-            address = request.form.get("address")
-            print(name, username, password, birthday, address)
-            user = Users(name=name, username=username, password=password, birthday=birthday, address=address)
-            try:
-                db.session.add(user)
-                db.session.commit()
-                return (f"User {name} successfully added to database...!")
-            except Exception as e:
-                return ("Exception raised! Operation was unsucessful...!")
-        
-        elif (request.form["button"] == "login"):
-            username = request.form.get("username")
-            password = request.form.get("password")
-            res = Users.query.get(username)
-            try:
-                if (username == res.username):
-                    if (password == res.password):
-                        print("before login", session)
-                        session["username"] = res.username
-                        print("after login", session)
-                        return render_template("user.html", username = username)
-            except Exception as e:
-                print(e)
-                return render_template("login.html", alert=True)
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        birthday = request.form.get("birthday")
+        address = request.form.get("address")
+        print(name, username, password, birthday, address)
+        user = Users(name=name, username=username, password=password, birthday=birthday, address=address)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return (f"User {name} successfully added to database...! + <b><a href = '/login'>click here to login</a></b>")
+        except Exception as e:
+            return ("Exception raised! Operation was unsucessful...!")
+
+@app.route("/login", methods=['POST','GET'])
+def login():
+    if request.method == "GET":
+        if 'username' in session:
+            username = session["username"]
+            return (f"<h1>Logged in as {username}</h1>" + "<b><a href = '/dropsession'>click here to log out</a></b>")
+        else:
+            return render_template("login.html")
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        res = Users.query.get(username)
+        try:
+            if (username == res.username):
+                if (password == res.password):
+                    print("before login", session)
+                    session["username"] = username
+                    print("after login", session)
+                    return render_template("user.html", username = username)
+        except Exception as e:
+            print(e)
+            return render_template("login.html", alert=True)
 
 @app.route("/admin", methods=["GET"])
 def admin():
@@ -83,10 +84,10 @@ def admin():
 
 @app.route("/dropsession")
 def logout():
-    print("before logout", session)
-    session.pop('username', None)
-    print("after logout", session)
-    return ("<h1>Successfully logged out...<h1>" + "<b><a href = '/register'>click here to login again</a></b>")
+    print("before pop", session)
+    session.pop("username")
+    print("after pop", session)
+    return ("<h1>Successfully logged out...<h1>" + "<b><a href = '/login'>click here to login again</a></b>")
 if __name__ == "__main__":
     with app.app_context():
         main()
--- Project1/project1/models.py
+++ Project1/project1/models.py
@@ -2,14 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 db = SQLAlchemy()
 
-# class Users(db.Model):
-#     __tablename__ = "USERS"
-#     name = db.Column(db.String, nullable=False)
-#     username = db.Column(db.String, primary_key=True)
-#     password = db.Column(db.String, nullable=False)
-#     birthday = db.Column(db.Date, nullable=False)
-#     address = db.Column(db.String, nullable=False)
-#     created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+class Users(db.Model):
+    __tablename__ = "USERS"
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String, nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
 
 class Books(db.Model):
     __tablename__ = "BOOKS"
